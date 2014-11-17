eea21074e3b08fbf638c0e6a0c2cf9b947f55b14
generic <generic@generic-pc.(none)>
2014-11-16 19:09:56 -0500

    Complete File Cipher

--- cipher.py
+++ cipher.py
@@ -1,6 +1,16 @@
+import os
+import sys
+import string
 
-def build_key(case):
-    test = string.%s % case
+def build_key1():
+    test = string.lowercase
+    g = []
+    for x in test:
+        g.append(x)
+    return g
+
+def build_key2():
+    test = string.uppercase
     g = []
     for x in test:
         g.append(x)
@@ -8,8 +18,6 @@ def build_key(case):
 
 def build_dict(key1, key2, shift):
     dic = {}
-    ciph1 = list(key1)
-    ciph2 = list(key2)
     for i in range(len(key1) - shift):
         dic[key1[i]] = key1[i + shift]
     for j in range(shift):
@@ -21,5 +29,55 @@ def build_dict(key1, key2, shift):
 
     return dic
 
+def get_file():
+    print "Please enter text file to be encrypted."
+    f = raw_input()
+    return f
 
+def get_shift(key1):
+    shift = 40
+    while not (int(shift) < len(key1)):
+        print "Please enter desired shift."
+        shift = raw_input()
+        
     
+    return int(shift)
+    
+def apply_dict(dic, filename):
+#    a = 0
+#    text = []    
+    with open('%s_encrypt.txt' % filename, 'w') as f:
+           
+        with open(filename, 'r') as o:
+            for line in o:
+#            text[a] = []
+                for char in line:
+                    if char in dic.keys():
+                        f.write(dic[char])
+                    else:
+                        f.write(char)
+    print "Encrypt done."
+
+def main():
+    key1 = build_key1()
+    key2 = build_key2()
+
+    shift = get_shift(key1)
+    
+    dic = build_dict(key1, key2, shift)
+
+    getf = True
+
+    while getf:
+        filename = get_file()
+        if not os.path.exists(filename):
+            print "That file does not exist"
+        else:
+            getf = False
+    
+    apply_dict(dic, filename)
+
+
+if __name__ == "__main__":
+
+    main()
